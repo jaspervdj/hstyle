f69b2bf0ea5a1b13442e4812f37bd1a899871256
Jasper Van der Jeugt <m@jaspervdj.be>
2011-11-11 12:39:13 +0100

    Write testRule using runRule

--- tests/HStyle/Tests/Util.hs
+++ tests/HStyle/Tests/Util.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 module HStyle.Tests.Util
     ( testRule
     , testRuleAccept
@@ -10,17 +11,19 @@ import Data.Text (Text)
 import HStyle
 import HStyle.Rule
 
--- | Check a module and list the errors
-testRule :: Rule -> Text -> [(Int, Text)]
-testRule (Rule selector checker _) text = do
+-- | Check a module and return the updated filestate
+testRule :: Rule -> Text -> FileState
+testRule rule text =
     let Right (md, block) = parseModule Nothing text
-    (x, range) <- selector md block
-    checker x block range
+        options           = Options True False
+        fs                = FileState "<unknown>" md block False True
+        (_, fs', _)       = runFileM (runRule rule) options fs
+    in fs'
 
 -- | Test if a rule accepts a certain module without problems
 testRuleAccept :: Rule -> Text -> Assertion
-testRuleAccept rule text = null (testRule rule text) @? "testRuleOk"
+testRuleAccept rule text = fileOk (testRule rule text) @? "testRuleOk"
 
 -- | Test if a rule rejects a certain module (e.g., at least one problem)
 testRuleReject :: Rule -> Text -> Assertion
-testRuleReject rule text = not (null $ testRule rule text) @? "testRuleReject"
+testRuleReject rule text = not (fileOk $ testRule rule text) @? "testRuleReject"
